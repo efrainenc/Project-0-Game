e5008964fcc153c2aa43cb0a6fec01b2648ff5d0
unknown <efraine387@gmail.com>
2022-11-11 16:19:12 -0500

    Added Function to increaseMetrics and set Metrics Intervals

--- main.js
+++ main.js
@@ -20,6 +20,9 @@ form.addEventListener("submit", (event) => {
     document.querySelector(".petName").textContent = `Name: ${tamagotchi.petName}`;
 });
 
+// Add buttons to the screen to feed your pet, 
+// turn off the lights, and play with your pet.
+
 // Pet dies if Hunger, Boredom, or Sleepiness hits 10
 // if(Hunger === 10 || Sleepiness === 10 || Boredom === 10){
 //     //tamagotchi dies
@@ -28,22 +31,30 @@ form.addEventListener("submit", (event) => {
 
 // set background to change on an interval to show time of day
 // Day lasts 5 min, Night last 5 min
-// 
 
-
-
-// Add buttons to the screen to feed your pet, 
-// turn off the lights, and play with your pet.
-
+//Function to Increase metrics
+function increaseMetric(metric){
+    if(metric === Hunger){
+        tamagotchi.Hunger++;
+    }else if(metric === Sleepiness){
+        tamagotchi.Sleepiness
+    }else if(metric === Boredom){
+        tamagotchi.Boredom++
+    }else if(metric === Age){
+        tamagotchi.Age++;
+    }
+}
 
 // set Hunger(1-10) to increase every 2 minutes
+setInterval(increaseMetric(Hunger), 120000)
 
-
-// set Sleepiness(1-10) to increase every 30sec lights arent off at night
+// set Sleepiness(1-10) to increase every 5min lights arent off at night
 // once asleep set time to day
+setInterval(increaseMetric(Sleepiness), 300000)
 
 
 // set Boredom(1-10) to increase every minute you dont play with pet
+setInterval(increaseMetric(Boredom), 60000)
 
-
-// set Age(Days) to increase .5 every 2.5 minutes
+// set Age(Days) to increase 1 every 5 minutes
+setInterval(increaseMetric(Age), 300000)
