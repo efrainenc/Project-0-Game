165b7dde5057948280856a5579e77adc3ade08d9
unknown <efraine387@gmail.com>
2022-11-11 17:48:13 -0500

    Changed how stored metrics are displayed

--- main.js
+++ main.js
@@ -7,6 +7,13 @@ const tamagotchi = {
     Boredom: 0,
     isAlive: true
 }
+let iterations = 0; // keep track of Interval iterations
+
+//Displaying stored metrics
+document.querySelector(".petAge").textContent = `Age: ${tamagotchi.Age}`;
+document.querySelector(".petHunger").textContent = `Hunger: ${tamagotchi.Hunger}`;
+document.querySelector(".petSleepiness").textContent = `Sleepiness: ${tamagotchi.Sleepiness}`;
+document.querySelector(".petBoredom").textContent = `Boredom: ${tamagotchi.Boredom}`;
 
 // Change Pet name
 const form = document.querySelector("#nameForm")
@@ -26,7 +33,7 @@ const feed = document.querySelector("#feed");
 const sleep = document.querySelector("#sleep");
 const play = document.querySelector("#play");
 
-feed.addEventListener("click", (event) => {
+feed.addEventListener("click", (event) => { // Decreases Hunger on button click
     event.preventDefault()
     if(tamagotchi.Hunger > 0){
         tamagotchi.Hunger--;
@@ -35,15 +42,29 @@ feed.addEventListener("click", (event) => {
 
 });
 
-sleep.addEventListener("click", (event) => {
+
+sleep.addEventListener("click", (event) => { // Decreases sleepiness on button click (also turn off lights/change bg to visually indicate sleep)
     event.preventDefault();
     if(tamagotchi.Sleepiness > 0){
         tamagotchi.Sleepiness--;
         document.querySelector(".petSleepiness").textContent = `Sleepiness: ${tamagotchi.Sleepiness}`;
+        const lightsOff = setInterval(setNightTime(), 1000) // turns off lights for 1 second
+        if(iterations === 1){
+            clearInterval(lightsOff);
+            document.body.style.backgroundImage = "url(https://i.imgur.com/hWCRjKy.png)"
+            iterations--;
+        }
+    }else {
+        alert(`${tamagotchi.petName} is not sleepy`)
+    }
+    // turn off lights and set night time
+    function setNightTime(){
+        iterations++;
+        document.body.style.backgroundImage = "url(https://i.imgur.com/3FA4btA.png)";
     }
 });
 
-play.addEventListener("click", (event) => {
+play.addEventListener("click", (event) => { // Decreases Boredom on button click (can also add animation to show pet playing)
     event.preventDefault()
     if(tamagotchi.Boredom > 0){
         tamagotchi.Boredom--;
@@ -56,10 +77,6 @@ play.addEventListener("click", (event) => {
 // if(Hunger === 10 || Sleepiness === 10 || Boredom === 10){
 //     //tamagotchi dies
 // }
-
-
-// set background to change on an interval to show time of day
-// Day lasts 5 min, Night last 5 min
 
 //Function to Increase metrics
 function increaseMetric(metric){
@@ -83,14 +100,14 @@ function increaseMetric(metric){
 }
 
 // set Hunger(1-10) to increase every 2 minutes
-setInterval(increaseMetric("hunger"), 2000)
+// setInterval(increaseMetric("hunger"), 2000)
 
 // set Sleepiness(1-10) to increase every 5min lights arent off at night
 // once asleep set time to day
-setInterval(increaseMetric("sleepiness"), 1000)
+// setInterval(increaseMetric("sleepiness"), 1000)
 
 // set Boredom(1-10) to increase every minute you dont play with pet
-setInterval(increaseMetric("boredom"), 5000)
+// setInterval(increaseMetric("boredom"), 5000)
 
 // set Age(Days) to increase 1 every 5 minutes
-setInterval(increaseMetric("age"), 10000)
+// setInterval(increaseMetric("age"), 10000)
