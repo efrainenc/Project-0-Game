eaf88c12dc6dc09e0af59a82e49a5c3c82ec69e9
unknown <efraine387@gmail.com>
2022-11-11 13:19:42 -0500

    Scenery set and pet Object Created

---
+++ main.js
@@ -0,0 +1,29 @@
+// create an object for my Tamagotchi
+const tamagotchi = {
+    petName: "Tamagotchi",
+    Age: 0,
+    Hunger: 0,
+    Sleepiness: 0,
+    Boredom: 0,
+}
+
+// Be able to change name of Pet 
+
+// set background to change on an interval to show time of day
+
+// Add buttons to the screen to feed your pet, 
+// turn off the lights, and play with your pet.
+// If Hunger, Boredom, or Sleepiness hits 10 pet dies
+
+
+// set Hunger(1-10) to increase every 2 minutes
+
+
+// set Sleepiness(1-10) to increase every 30sec lights arent off at night
+// once asleep set time to day
+
+
+// set Boredom(1-10) to increase every minute you dont play with pet
+
+
+// set Age(Days) to increase .5 every 2.5 minutes
