bf40b907a483a3d0527fbde5542e2f0958de3af1
unknown <efraine387@gmail.com>
2022-11-13 15:09:17 -0500

    function for checkAlive added

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const tamagotchi = {
     petName: "Tamagotchi",
     Age: 0,
     Hunger: 0,
-    Sleepiness: 5,
-    Boredom: 0,
+    Sleepiness: 0,
+    Boredom: 9,
     isAlive: true,
     sleepTime: 0
 }
@@ -75,7 +75,7 @@ sleep.addEventListener("click", (event) => {
     }else if(sleep.value==="OFF" || tamagotchi.Sleepiness !== 0){
         sleep.value = "ON";
         console.log(sleep.value)
-        clearInterval(tamagotchi.sleepTime);
+        clearInterval(tamagotchi.sleepTime);// stop decrease when lights on
         document.querySelector("#sleep").innerHTML = "Sleep💤" // change button back to sleep
         // changes back to day time image
         document.body.style.backgroundImage = "url(https://i.imgur.com/hWCRjKy.png)";
@@ -98,42 +98,51 @@ play.addEventListener("click", (event) => {
 
 
 // Pet dies if Hunger, Boredom, or Sleepiness hits 10
-if(tamagotchi.Hunger === 10 || tamagotchi.Sleepiness === 10 || tamagotchi.Boredom === 10){
-    //tamagotchi dies
-    tamagotchi.isAlive = false;
-    alert(`Your Tamagotchi ${tamagotchi.petName} has passed away`)
+function checkAlive(){
+    if(tamagotchi.Hunger === 10 || tamagotchi.Sleepiness === 10 || tamagotchi.Boredom === 10){
+        //tamagotchi dies
+        tamagotchi.isAlive = false;
+        clearInterval(increaseHunger);
+        clearInterval(increaseSleepy);
+        clearInterval(increaseBoredom);
+        clearInterval(increaseAge);
+        alert(`Your Tamagotchi "${tamagotchi.petName}" has passed away`);
+    } 
 }
-
 //Function to Increase metrics
 function increaseMetric(metric){
     if(metric === "hunger"){
         tamagotchi.Hunger++;
         document.querySelector(".petHunger").textContent = `Hunger: ${tamagotchi.Hunger}`;
         console.log(tamagotchi.Hunger)
+        checkAlive()
     }else if(metric === "sleepiness"){
         tamagotchi.Sleepiness++;
         document.querySelector(".petSleepiness").textContent = `Sleepiness: ${tamagotchi.Sleepiness}`;
         console.log(tamagotchi.Sleepiness)
+        checkAlive()
     }else if(metric === "boredom"){
         tamagotchi.Boredom++;
         document.querySelector(".petBoredom").textContent = `Boredom: ${tamagotchi.Boredom}`;
         console.log(tamagotchi.Boredom)
+        checkAlive()
     }else if(metric === "age"){
         tamagotchi.Age++;
         document.querySelector(".petAge").textContent = `Age: ${tamagotchi.Age}`;
         console.log(tamagotchi.Age)
+        checkAlive()
     }
 }
 
 // set Hunger(1-10) to increase every 2 minutes
-setInterval(increaseMetric, 120000, "hunger")
+let increaseHunger = setInterval(increaseMetric, 120000, "hunger");
 
 // set Sleepiness(1-10) to increase every 5min lights arent off at night
 // once asleep set time to day
-setInterval(increaseMetric, 300000, "sleepiness")
+let increaseSleepy = setInterval(increaseMetric, 300000, "sleepiness");
 
 // set Boredom(1-10) to increase every 1.5 minutes you dont play with pet
-setInterval(increaseMetric, 90000, "boredom")
+let increaseBoredom = setInterval(increaseMetric, 90000, "boredom");
 
 // set Age(Days) to increase 1 every 5 minutes
-setInterval(increaseMetric, 300000,"age")
+let increaseAge = setInterval(increaseMetric, 300000,"age");
